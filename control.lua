edbc660ddee7c4116f547d9a34500b9cf8747f81
Valerate <kassyesp@hotmail.com>
2016-07-26 18:37:25 +0200

    Init release
    Doesnt even work slighlty

---
+++ control.lua
@@ -0,0 +1,63 @@
+function writeDebug(message)
+      for key, player in pairs(game.players) do
+         player.print(tostring(message))
+      end
+end
+
+
+script.on_event("decrease-generator-priority", function(event)
+	writeDebug( "Decrease")
+	local selection = game.players[event.player_index].selected
+	if selection and selection.type == "generator" then
+		DecreasePriority(selection, event)
+	end
+end)
+
+script.on_event("increase-generator-priority", function(event)
+	writeDebug( "Increase")
+	local selection = game.players[event.player_index].selected
+	if selection and selection.type == "generator" then
+		IncreasePriority(selection, event)
+	end
+end)
+
+function IncreasePriority(entity, event)
+	writeDebug("before")
+	local Priority = entity.energy_source.usage_priority
+	writeDebug("after")
+	local UsageList = { "primary", "secondary", "terciary" }
+	local New = "primary"
+		if Priority == "primary" then 
+			print("The generator is now secondary")
+			New =  "secondary"
+		elseif Priority == "secondary" then
+			print("The generator is now terciary")
+			New =  "terciary"
+		elseif Priority == "terciary" then
+			print("The generator is already terciary")
+			New =  "terciary"
+		else
+			print("You Done fuck'd")
+		end
+	entity.entity.energy_source.usage_priority = New
+end
+
+function DecreasePriority(entity, event)
+	local Priority = entity.energy_source.usage_priority
+	local UsageList = { "primary", "secondary", "terciary" }
+	local New = "primary"
+		if Priority == "primary" then 
+			print("The generator is already primary")
+			New =  "primary"
+		elseif Priority == "secondary" then
+			print("The generator is now primary")
+			New =  "primary"
+		elseif Priority == "terciary" then
+			print("The generator is now secondary")
+			New =  "secondary"
+		else
+			print("You Done fuck'd")
+		end
+	entity.entity.energy_source.usage_priority = New
+
+end
---
+++ data.lua
@@ -0,0 +1,14 @@
+data:extend({
+  {
+    type = "custom-input",
+    name = "decrease-generator-priority",
+    key_sequence = "SHIFT + L",
+    consuming = "all"
+  },
+  {
+    type = "custom-input",
+    name = "increase-generator-priority",
+    key_sequence = "SHIFT + O",
+    consuming = "all"
+  },
+})
